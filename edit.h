6b7f1340f8710450006bea606e6f3049d228325e
Ubuntu <ubuntu@ip-172-31-3-190.ap-northeast-2.compute.internal>
2016-11-30 01:20:52 +0000

    SooJin

--- edit.h
+++
@@ -1,6 +0,0 @@
-#include <ncurses.h>
-void setPrompt(char *prompt, WINDOW* wnd);
-void insertModeEnter(WINDOW* wnd);
-void moveCursor(int ch, int dy, int dx);
-void insertModeBackspace(WINDOW* wnd, int dy, int dx);
-void insertModePrintChar(WINDOW* wnd, int ch);
